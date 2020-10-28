e6c6cf57ed4437bda04ec90d8666c3ed247fecf0
Gal Birkman <galbirkman@gmail.com>
2020-10-28 09:46:11 +0000

    Added config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-SENDER = 'no_reply@galbirkman.com'
-USER = 'galbirkman@gmail.com'
+import os
+API_KEY = 'eb185261abmshce945baa82d97a1p16fe8cjsn9e7fe2f909d2'
+LOG_PATH = r"{}\DEBUG.log".format(os.path.dirname(os.path.realpath(__file__)))
+STOCK_SYMBOL = "FVRR"
+STOCK_REGION = "US"
+ICON_PATH = r"{}\bull.ico".format(os.path.dirname(os.path.realpath(__file__)))
+CHECK_INTERVAL = 60 # Check every x seconds
--- get-fiverr.py
+++ get-fiverr.py
@@ -5,21 +5,22 @@ from datetime import datetime
 from win10toast import ToastNotifier
 import time
 import logging
+import config
 
 logging.basicConfig(
     level=logging.INFO,
-    handlers=[ logging.FileHandler(r"D:\DevOps\python\rapidAPI\yahoo-finance-api\prices.log"), logging.StreamHandler() ],
+    handlers=[ logging.FileHandler(config.LOG_PATH), logging.StreamHandler() ],
     format='%(asctime)s:%(levelname)s: %(message)s', 
 )
 
 def get_price():
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail"
 
-    querystring = {"region":"US","symbol":"FVRR"}
+    querystring = {"region":config.STOCK_REGION,"symbol":config.STOCK_SYMBOL}
 
     headers = {
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-        'x-rapidapi-key': "eb185261abmshce945baa82d97a1p16fe8cjsn9e7fe2f909d2"
+        'x-rapidapi-key': config.API_KEY
         }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -29,7 +30,7 @@ def get_price():
 
 def notify(price):
     toaster = ToastNotifier()
-    toaster.show_toast("FVRR Stock Price Changed!","THE PRICE IS {}".format(price),icon_path=r"D:\DevOps\python\rapidAPI\yahoo-finance-api\bull.ico",duration=15)
+    toaster.show_toast("{} Stock Price Changed!".format(config.STOCK_SYMBOL),"THE PRICE IS {}".format(price),icon_path=config.ICON_PATH,duration=15)
 
 price = get_price()
 notify(price)
@@ -38,4 +39,4 @@ while True:
     if new_price != price and price is not None:
         notify(new_price)
         price = new_price
-    time.sleep(60)
+    time.sleep(config.CHECK_INTERVAL)
